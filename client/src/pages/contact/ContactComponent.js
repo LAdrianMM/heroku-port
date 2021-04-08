83c940243a908f1b2259e458609131f5df43d4b5
LAdrianMM <ladrianmm@gmail.com>
2021-04-07 20:54:11 -0400

    portfolio update and loader

--- client/src/pages/contact/ContactComponent.js
+++ client/src/pages/contact/ContactComponent.js
@@ -6,9 +6,16 @@ import { Fade } from "react-reveal";
 import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 import { style } from "glamor";
-import { Paper, makeStyles, TextField, Button, CircularProgress, Snackbar } from '@material-ui/core'
-import { useFormik } from 'formik'
-import MuiAlert from '@material-ui/lab/Alert'
+import {
+  Paper,
+  makeStyles,
+  TextField,
+  Button,
+  CircularProgress,
+  Snackbar,
+} from "@material-ui/core";
+import { useFormik } from "formik";
+import MuiAlert from "@material-ui/lab/Alert";
 
 const ContactData = contactPageData.contactSection;
 // const blogSection = contactPageData.blogSection;
@@ -17,72 +24,71 @@ function Contact(props) {
   const openRepoinNewTab = (url) => {
     let win = window.open(url, "_blank");
     win.focus();
-  }
-  const classes = useStyles()
+  };
+  const classes = useStyles();
   const theme = props.theme;
-  const [ sending, setSending ] = useState(false)
-  const [ success, setSuccess ] = useState(false)
-  const [ error, setError ] = useState(false)
-  
-  const validate = values => {
+  const [sending, setSending] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const validate = (values) => {
     const errors = {};
 
     if (!values.name) {
-      errors.name = '- required';
+      errors.name = "- required";
     }
-    
+
     if (!values.email) {
-      errors.email = '- required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = '- invalid email address';
+      errors.email = "- required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "- invalid email address";
     }
 
     if (!values.subject) {
-      errors.subject = '- required';
+      errors.subject = "- required";
     }
 
     if (!values.message) {
-      errors.message = '- required';
+      errors.message = "- required";
     }
-  
+
     return errors;
   };
-  
+
   const formik = useFormik({
     initialValues: {
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
     },
     validate,
-    onSubmit : (values, { resetForm }) => {
-      setSending(true)
-      fetch('/api/sendMail', {
-        method: 'POST',
+    onSubmit: (values, { resetForm }) => {
+      setSending(true);
+      fetch("/api/sendMail", {
+        method: "POST",
         headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(values)
-	
-    })
-    .then(response => {
-      setSuccess(true)
-      setSending(false)
-      return response.json()
-    })
-    .catch(err => {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      })
+        .then((response) => {
+          setSuccess(true);
+          setSending(false);
+          return response.json();
+        })
+        .catch((err) => {
+          setError(true);
+          setSending(false);
+          console.log(err);
+        });
 
-      setError(true)
-      setSending(false)
-      console.log(err)
-    })
-
-    resetForm({})
-  }
-
-})
+      resetForm({});
+    },
+  });
 
   const styles = style({
     backgroundColor: `${theme.accentColor}`,
@@ -91,20 +97,20 @@ function Contact(props) {
     },
   });
   const handleClick = () => {
-    formik.handleSubmit()
-  }
+    formik.handleSubmit();
+  };
   function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
   }
   const handleCloseSuccess = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setSuccess(false);
   };
   const handleCloseError = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -131,7 +137,11 @@ function Contact(props) {
                 {ContactData["description"]}
               </p>
               <div className={classes.formWrapper}>
-                <Paper className={classes.formInfo} variant='outlined' elevation={3}>
+                <Paper
+                  className={classes.formInfo}
+                  variant="outlined"
+                  elevation={3}
+                >
                   <div>
                     <h2>Address</h2>
                     <h3>{ContactData["address"]}</h3>
@@ -145,70 +155,107 @@ function Contact(props) {
                     <h3>{ContactData["email"]}</h3>
                   </div>
                 </Paper>
-                <Paper className={classes.formInput} variant='outlined' elevation={3}>
+                <Paper
+                  className={classes.formInput}
+                  variant="outlined"
+                  elevation={3}
+                >
                   <form onSubmit={formik.handleSubmit} className={classes.form}>
-                    <TextField 
-                      id='name' 
-                      name='name' 
-                      onChange={formik.handleChange} 
-                      value={formik.values.name || ''}
-                      type='text' 
-                      className={classes.inputName} 
-                      label={`Name * ${formik.touched.name && formik.errors.name ? formik.errors.name : ''}`} 
-                      variant="filled" 
+                    <TextField
+                      id="name"
+                      name="name"
+                      onChange={formik.handleChange}
+                      value={formik.values.name || ""}
+                      type="text"
+                      className={classes.inputName}
+                      label={`Name * ${
+                        formik.touched.name && formik.errors.name
+                          ? formik.errors.name
+                          : ""
+                      }`}
+                      variant="filled"
                       onBlur={formik.handleBlur}
-                      error={formik.touched.name && formik.errors.name ? true : false}
-                    />
-                    <TextField 
-                      id='email' 
-                      name='email' 
-                      onChange={formik.handleChange} 
-                      value={formik.values.email}
-                      type='email' 
-                      className={classes.inputEmail} 
-                      label={`Email * ${formik.touched.email && formik.errors.email ? formik.errors.email : ''}`} 
-                      variant="filled" 
-                      onBlur={formik.handleBlur}
-                      error={formik.touched.email && formik.errors.email ? true : false} 
-                    />
-                    <TextField 
-                      id='subject' 
-                      name='subject' 
-                      onChange={formik.handleChange} 
-                      value={formik.values.subject}
-                      type='text' 
-                      className={classes.inputSubject} 
-                      label={`Subject * ${formik.touched.subject && formik.errors.subject ? formik.errors.subject : ''}`} 
-                      variant="filled" 
-                      onBlur={formik.handleBlur}
-                      error={formik.touched.subject && formik.errors.subject ? true : false} 
+                      error={
+                        formik.touched.name && formik.errors.name ? true : false
+                      }
                     />
                     <TextField
-                      id='message' 
-                      name='message' 
-                      onChange={formik.handleChange} 
+                      id="email"
+                      name="email"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      type="email"
+                      className={classes.inputEmail}
+                      label={`Email * ${
+                        formik.touched.email && formik.errors.email
+                          ? formik.errors.email
+                          : ""
+                      }`}
+                      variant="filled"
+                      onBlur={formik.handleBlur}
+                      error={
+                        formik.touched.email && formik.errors.email
+                          ? true
+                          : false
+                      }
+                    />
+                    <TextField
+                      id="subject"
+                      name="subject"
+                      onChange={formik.handleChange}
+                      value={formik.values.subject}
+                      type="text"
+                      className={classes.inputSubject}
+                      label={`Subject * ${
+                        formik.touched.subject && formik.errors.subject
+                          ? formik.errors.subject
+                          : ""
+                      }`}
+                      variant="filled"
+                      onBlur={formik.handleBlur}
+                      error={
+                        formik.touched.subject && formik.errors.subject
+                          ? true
+                          : false
+                      }
+                    />
+                    <TextField
+                      id="message"
+                      name="message"
+                      onChange={formik.handleChange}
                       value={formik.values.message}
-                      className= {classes.inputMessage}
-                      label={`Your Messagge Here... * ${ formik.touched.message && formik.errors.message ? formik.errors.message : ''}`} 
+                      className={classes.inputMessage}
+                      label={`Your Messagge Here... * ${
+                        formik.touched.message && formik.errors.message
+                          ? formik.errors.message
+                          : ""
+                      }`}
                       multiline
                       rows={6}
                       variant="filled"
                       onBlur={formik.handleBlur}
-                      error={formik.touched.message && formik.errors.message ? true : false}
+                      error={
+                        formik.touched.message && formik.errors.message
+                          ? true
+                          : false
+                      }
                     />
                     <div className={classes.buttonWrapper}>
                       <Button
-                      disabled={sending}
-                        typy='submit'
+                        disabled={sending}
+                        typy="submit"
                         {...styles}
                         className={classes.formButton}
                         onClick={() => handleClick()}
                       >
-                        {
-                          sending ? 'Sending ...' : 'Send Messagge'
-                        }
+                        {sending ? "Sending ..." : "Send Messagge"}
                       </Button>
-                      {sending && <CircularProgress size={30} className={classes.progress} />}
+                      {sending && (
+                        <CircularProgress
+                          size={30}
+                          className={classes.progress}
+                        />
+                      )}
                     </div>
                   </form>
                 </Paper>
@@ -216,142 +263,151 @@ function Contact(props) {
               <SocialMedia />
               <br />
               <br />
-              <button 
+              <button
                 onClick={() => openRepoinNewTab(greeting.resumeLink)}
-                {...styles} 
-                className="general-btn" 
-                >
+                {...styles}
+                className="general-btn"
+              >
                 See my Resume
               </button>
             </div>
           </div>
         </Fade>
       </div>
-      <Snackbar 
-        open={success} 
-        autoHideDuration={4000} 
+      <Snackbar
+        open={success}
+        autoHideDuration={4000}
         onClose={handleCloseSuccess}
-        anchorOrigin={{vertical: 'bottom', horizontal:'right'}}
-        >
-          <Alert severity="success">Yeah!, your messagge has been sent!!</Alert>
-        </Snackbar>
-        <Snackbar 
-        open={error} 
-        autoHideDuration={4000} 
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert severity="success">Yeah!, your messagge has been sent!!</Alert>
+      </Snackbar>
+      <Snackbar
+        open={error}
+        autoHideDuration={4000}
         onClose={handleCloseError}
-        anchorOrigin={{vertical: 'bottom', horizontal:'right'}}
-        >
-          <Alert severity="error">Ups!, An error has ocurred.</Alert>
-        </Snackbar>
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert severity="error">Ups!, An error has ocurred.</Alert>
+      </Snackbar>
     </div>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   form: {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
     gap: theme.spacing(2),
     marginTop: theme.spacing(2),
-    
-    [theme.breakpoints.up('md')]: {
+
+    [theme.breakpoints.up("md")]: {
       marginTop: 0,
-      width: '700px'
-    }
+      width: "700px",
+    },
   },
   buttonWrapper: {
-    gridColumn: '1 / 3',
-    height: '50px',
+    gridColumn: "1 / 3",
+    height: "50px",
     marginTop: theme.spacing(5),
-    position: 'relative'
-    
+    position: "relative",
   },
   formButton: {
-    backgroundColor: 'hsl(214, 96%, 70%)',
-    height: '100%',
-    outline: 'none',
-    border: 'none',
-    borderRadius: '10px',
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: '18px',
-    textTransform: 'capitalize',
-    width: '100%',
+    backgroundColor: "hsl(214, 96%, 70%)",
+    height: "100%",
+    outline: "none",
+    border: "none",
+    borderRadius: "10px",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "18px",
+    textTransform: "capitalize",
+    width: "100%",
 
-    '&:hover': {
-      backgroundColor: 'hsl(214, 96%, 70%)',
-    }
+    "&:hover": {
+      backgroundColor: "hsl(214, 96%, 70%)",
+    },
+    "&:focus": {
+      backgroundColor: "hsl(214, 96%, 70%)",
+    },
   },
   progress: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: '-17px',
-    marginLeft: '60px',
-    color: 'red',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: "-17px",
+    marginLeft: "60px",
+    color: "red",
 
-    '& svg': {
-      backgroundColor: 'hsl(214, 96%, 70%)'
-    }
+    "& svg": {
+      backgroundColor: "hsl(214, 96%, 70%)",
+
+      "&:focus": {
+        backgroundColor: "hsl(214, 96%, 70%)",
+      },
+      "&:hover": {
+        backgroundColor: "hsl(214, 96%, 70%)",
+      },
+    },
   },
   formWrapper: {
     marginBottom: theme.spacing(4),
-    
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    }
+
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
   },
   formInfo: {
-    minHeight: '400px',
-    width: '100%',
-    borderRadius: '20px',
+    minHeight: "400px",
+    width: "100%",
+    borderRadius: "20px",
     marginRight: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
     padding: theme.spacing(2),
 
-    '&:hover': {
-      backgroundColor: 'hsl(214, 100%, 90%)'
+    "&:hover": {
+      backgroundColor: "hsl(214, 100%, 90%)",
     },
-    [theme.breakpoints.up('md')]: {
-      width: '50%',
-    }
+    [theme.breakpoints.up("md")]: {
+      width: "50%",
+    },
   },
   formInput: {
-    borderRadius: '20px',
-    backgroundColor: 'transparent',
-    width: '100%',
-    
-    '& div': {
-      backgroundColor: '#fff',
-      borderRadius: '10px',
+    borderRadius: "20px",
+    backgroundColor: "transparent",
+    width: "100%",
 
-      '&:hover': {
-        backgroundColor: 'hsl(214, 100%, 90%)'
+    "& div": {
+      backgroundColor: "#fff",
+      borderRadius: "10px",
+
+      "&:hover": {
+        backgroundColor: "hsl(214, 100%, 90%)",
       },
     },
   },
   inputEmail: {
-    gridColumn: '1 /3',
+    gridColumn: "1 /3",
 
-    [theme.breakpoints.up('sm')]: {
-      gridColumn: '2 / 3',
+    [theme.breakpoints.up("sm")]: {
+      gridColumn: "2 / 3",
     },
   },
   inputName: {
-    gridColumn: '1 /3',
+    gridColumn: "1 /3",
 
-    [theme.breakpoints.up('sm')]: {
-      gridColumn: '1 / 2',
+    [theme.breakpoints.up("sm")]: {
+      gridColumn: "1 / 2",
     },
   },
   inputMessage: {
-    gridColumn: '1 / 3',
+    gridColumn: "1 / 3",
   },
   inputSubject: {
-    gridColumn: '1 / 3',
+    gridColumn: "1 / 3",
   },
-}))
+}));
 
 export default Contact;
